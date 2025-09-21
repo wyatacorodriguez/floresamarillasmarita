da941720d3f7df379e3b1822856a28470771fcd2
wyatacorodriguez <willianyataco@gmail.com>
2025-09-21 18:28:13 -0500

    Update anim.js
    ed
    
    Signed-off-by: wyatacorodriguez <willianyataco@gmail.com>

--- anim.js
+++ anim.js
@@ -15,22 +15,6 @@ var lyricsData = [
   { text: "espero te guste mucho, ya sea copiado o editado", time: 59 },
   { text: "pero lo hice para ti", time: 67 },
   { text: "con mucho amor mi gordita", time: 72 },
-  { text: "Silence Let me see what it was", time: 78 },
-  { text: "I only want to live in clouds", time: 83 },
-  { text: "Where I'm now? I don't know why", time: 91 },
-  { text: "Nice butterflies in my hands", time: 97 },
-  { text: "Too much light for twilight", time: 104 },
-  { text: "In the mood for the flowers love", time: 108 },
-  { text: "At the time", time: 144 },
-  { text: "The whisper of birds", time: 148 },
-  { text: "Lonely before the sun cried", time: 153 },
-  { text: "Fell from the sky", time: 158 },
-  { text: "Like water drops", time: 164 },
-  { text: "Where I'm now? I don't know why", time: 169 },
-  { text: "Nice butterflies in my hands", time: 176 },
-  { text: "Too much light for twilight", time: 183 },
-  { text: "In the mood for the flowers", time: 188 },
-  { text: "Love.", time: 140 },
 ];
 
 // Animar las letras
